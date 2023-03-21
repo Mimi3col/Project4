4d34e6f0a3ed0589d6f09dce19dc244c2ed398d3
Amanda Coleman <acoleman8097@floridapoly.edu>
2023-03-20 21:36:21 -0400

    got images to show

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import React, { useState } from 'react'
 import './App.css'
-const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+const API_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
+const API_URL = `https://api.thecatapi.com/v1/`;
 
 
 function App() {
+  const [currentImage, setCurrentImage] = useState("")
+  const [id, setId] = useState("")
+
+function getCatImages() {
+  const url = `${API_URL}images/search`;
+
+  fetch(url, {
+    headers: {
+      "x-api-key": API_KEY,
+    },
+  })
+  .then(res=>res.json())
+  .then(data=>setCurrentImage(data[0].url))
+  .then(data=>setId(data[0].id))
+}
+
+
 
 
   return (
     <div className='App'>
-      hello world
+    <h1>My Cat Images</h1>
+      <img className='catImage' src={currentImage}></img>
+     <br></br>
+    <button onClick={getCatImages}>Click for new image</button>
     </div> 
   )
 }
