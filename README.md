93b71a9005e54df0871539560e1432ee34449f1e
Amanda Coleman <acoleman8097@floridapoly.edu>
2023-03-22 11:44:45 -0400

    all requirements met

--- README.md
+++ README.md
@@ -11,11 +11,14 @@ Time spent: **4** hours spent in total
 The following **required** functionality is completed:
 
 - [x] **Clicking a button creates a new API fetch request and displays at least three attributes from the returned JSON data**
+    - Their are four being displayed: Image, name, origin, description
 - [x] **Only one item/API call is viewable at a time**
+    - Only one set of information is being shown at a time
 - [x] **API calls appear random to the user**
+    - each time the user selects the click for new image button a new API call is retrieved
 - [x] **At least one image is displayed per API call**
-- [ ] **Clicking on a displayed value for one attribute adds it to a displayed ban list**
-- [ ] **Attributes on the ban list prevent further images/API results with that attribute from being displayed**
+- [x] **Clicking on a displayed value for one attribute adds it to a displayed ban list**
+- [x] **Attributes on the ban list prevent further images/API results with that attribute from being displayed**
 
 The following **optional** features are implemented:
 
@@ -32,9 +35,12 @@ Here's a walkthrough of implemented user stories:
 
 ![My Video Walk Through](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmJiMmE1OTY0MzA4MWFhMThiMDU3M2Q2N2I1ODA5M2ZhZjg2Y2I0NCZjdD1n/N0RYMESzXfMqROPmj5/giphy.gif)
 
+
+
+
 ## Notes
 
-it was a trial trying to understand how the fetch worked, but once I understood how I would be able to read the data that was returned from the api. I was able to push through more of the project. I was unable to finish the code completely but I understand where I need to go next with it in order to complete the last two parts of the rubric. 
+Using fetch I bring over an array from the API and take the information I need to display. There are four data points that change on every click of the new image button: Image, Origin, Name, Description. When a user clicks on either the Name or Origin of the cat that information will then be added to the bans list. To change the ban simply click on the new name to ban and the old one will be replaced. The same method will work for the cat origin. In order to clear the bans list the user will need to select the clear bans button.
 
 ## License
 
--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,23 @@ const API_URL = `https://api.thecatapi.com/v1/`;
 
 
 function App() {
-  const [currentImage, setCurrentImage] = useState("")
-  const [catName, setCatName] = useState("")
-  const [catOrgin, setOrgin] = useState("")
-  const [description, setDiscription] = useState("")
+
+  const [catCard, setCatCard] = useState({
+    imageUrl:"",
+    catName:"",
+    catOrigin:"",
+    description:""
+  })
+
+  const [banned, setBanned] = useState({
+    catName:"Please select a Name to ban",
+    catOrigin:"Please Select an Origin to ban"
+  })
+
+
+  const [clicked, setClicked] = useState(0)
+
+
 
 function getCatImages() {
   const url = `${API_URL}images/search?has_breeds=1`;
@@ -19,30 +32,70 @@ function getCatImages() {
     },
   })
   .then(res=>res.json())
-  .then(data=>{
-    setCurrentImage(data[0].url),
-      setCatName(data[0].breeds[0].name),
-      setOrgin(data[0].breeds[0].origin),
-      setDiscription(data[0].breeds[0].description);
-  })
+  .then(data=>{(data[0].breeds[0].name === banned.catName || data[0].breeds[0].origin === banned.catOrigin)? getCatImages():
+    setCatCard({
+      imageUrl: data[0].url,
+      catName: data[0].breeds[0].name,
+      catOrigin: data[0].breeds[0].origin,
+      description: data[0].breeds[0].description
+    });
+    })
 }
 
+function checkBan(name,origin){
+    console.log(name === banned.catName)
+    console.log(origin === banned.catOrigin)
+    if(origin === banned.catOrigin || name === banned.catName){
+      
+      getCatImages()
+    }
 
+} 
   return (
     <div className="App">
-      <h1 className='Title'>Random Cat Images and Information</h1> <br></br>
-      <div className='CardContainer'>
-      <div className="card">
-        <img className="catImage" src={currentImage}></img>
-        <p className='info'> Type of cat:  {catName}</p>  <p className='info'> Origin of cat:   {catOrgin}</p>
-        <p className='info'> {description}  </p>
-        <button onClick={getCatImages}>Click for new image</button>
-      </div>
-      <br></br>
-      <div className="card">
-        <h1>Bans List</h1>
-        <p>Origins or breads that should not appear</p>
-      </div>
+      <h1 className="Title">Random Cat Images and Information</h1> <br></br>
+      <div className="CardContainer">
+        <div className="card">
+          <img className="catImage" src={catCard.imageUrl}></img>
+          <div className="info">
+            Type of cat:
+            <button
+              onClick={() =>
+                setBanned({
+                  catName: catCard.catName,
+                  catOrigin: banned.catOrigin,
+                })
+              }>
+              {catCard.catName}
+            </button>
+            <span>
+              <a style={{ marginLeft: "3rem" }}></a>
+            </span>
+            Origin of a cat
+            <button
+              onClick={() =>
+                setBanned({
+                  catOrigin: catCard.catOrigin,
+                  catName: banned.catName,
+                })
+              }>
+              {catCard.catOrigin}
+            </button>
+          </div>
+          <p className="info"> {catCard.description} </p>
+          <button onClick={getCatImages}>Click for new image</button>
+        </div>
+        <br></br>
+        <div className="card">
+          <h1>Bans List</h1>
+          <br></br>
+          <h2>{banned.catName}</h2>
+          <br></br>
+          <h2>{banned.catOrigin}</h2>
+          <button className='clearbans' onClick={() => setBanned({ catName: null, catOrigin: null })}>
+            Clear all Bans
+          </button>
+        </div>
       </div>
     </div>
   );
